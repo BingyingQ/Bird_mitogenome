3505171c1c78e7454af1a27388b97772714c2d11
BingyingQ <134666372+BingyingQ@users.noreply.github.com>
2024-03-10 23:38:05 +0800

    Update 3.extract_country_from_gb.py

--- 3.extract_country_from_gb.py
+++ 3.extract_country_from_gb.py
@@ -3,6 +3,7 @@ from Bio import SeqIO
 import csv
 from datetime import datetime
 import argparse
+import pandas as pd
 
 def extract_country_info(genbank_folder, output_file):
     with open(output_file, 'w', newline='', encoding='utf-8') as csvfile:
@@ -54,11 +55,35 @@ def extract_country_info(genbank_folder, output_file):
 
                     csv_writer.writerow([species_name, accession, country, source])
 
+def analyze_china_presence(input_file, output_file):
+    df = pd.read_csv(input_file, sep="\t")
+
+    # 提取物种名的前两部分，创建新的列
+    df['Species_prefix'] = df['Species'].apply(lambda x: ' '.join(x.split()[:2]))
+
+    # 按前两部分物种名分组
+    grouped_species = df.groupby('Species_prefix')
+
+    # 创建结果的DataFrame
+    result_df = pd.DataFrame(columns=['Species_prefix', 'China_presence'])
+
+    # 遍历每个物种的所有行
+    for species_prefix, group in grouped_species:
+        has_china_sequence = 'Yes' if 'China' in group['Country'].values else 'No'
+        result_df = result_df.append({'Species_prefix': species_prefix, 'China_presence': has_china_sequence}, ignore_index=True)
+
+    # 输出结果到新的CSV文件
+    result_df.to_csv(output_file, sep="\t", index=False)
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Extract country information from GenBank files.')
+    parser = argparse.ArgumentParser(description='Extract and analyze country information from GenBank files.')
     parser.add_argument('-i', '--input_folder', default='./downloaded_mtgenomes_gb/', help='Path to the GenBank folder')
-    parser.add_argument('-o', '--output_file', default='country_output.tsv', help='Path to the output CSV file')
+    parser.add_argument('-o', '--output_file', default='country_output.csv', help='Path to the output CSV file')
+    parser.add_argument('-c', '--china_output_file', default='china_or_not_result.csv', help='The CSV file presented whether these species contains sequences from China')
     args = parser.parse_args()
 
+    # 调用提取国家信息的函数
     extract_country_info(args.input_folder, args.output_file)
 
+    # 调用分析China是否存在的函数
+    analyze_china_presence(args.output_file, args.china_output_file)
